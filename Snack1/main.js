c9f09a7133b232580bca0674eb555dbff4c3b30f
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-01-30 15:00:29 +0100

    added ex-1

--- Snack1/main.js
+++ Snack1/main.js
@@ -1,2 +1,12 @@
 // Snack1
 // Ottieni un numero casuale in un range specificato. Chiedi all'utente il valore minimo ed il valore massimo del range in cui generare il numero.
+
+let userNumbers = [];
+
+let maxNumber = parseInt(prompt("Inserisci il valore massimo del range"));
+let minNumber = parseInt(prompt("Inserisci il valore minimo del range"));
+
+userNumbers.push(maxNumber);
+userNumbers.push(minNumber);
+
+console.log(userNumbers);
